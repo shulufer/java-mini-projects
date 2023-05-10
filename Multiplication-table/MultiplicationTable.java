17ee530e8dd2cb7d9b9812bc299310e1056ce615
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-05-10 17:38:48 +0200

    Multiplication Table

---
+++ Multiplication-table/MultiplicationTable.java
@@ -0,0 +1,13 @@
+public class MultiplicationTable {
+
+  public static void main(String args[]) {
+
+    for (int i = 1; i < 10; i++) {
+      System.out.println("*****************");
+      for (int j = 1; j < 10; j++) {
+        System.out.println(i + " * " + j + " = " + i*j);
+      }
+    }
+  }
+
+}
