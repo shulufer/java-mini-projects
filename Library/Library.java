7304d215653b5664490e3651dc127a6bca8ab350
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-07-13 21:43:34 +0200

    created library class with getter and setter

---
+++ Library/Library.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+
+public class Library {
+
+  private int numberOfBook;
+  private String name;
+  private Map<String, Integer> bookGenresMap;
+
+
+
+  public int getNumberOfBook() {
+    return this.numberOfBook;
+  }
+
+  public void setNumberOfBook(int numberOfBook) {
+    this.numberOfBook = numberOfBook;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Map<String,Integer> getBookGenresMap() {
+    return this.bookGenresMap;
+  }
+
+  public void setBookGenresMap(Map<String,Integer> bookGenresMap) {
+    this.bookGenresMap = bookGenresMap;
+  }
+
+
+}
