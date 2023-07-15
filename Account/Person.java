c61bf6e511356f0a1b8dec638de144e7e2c672a6
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-07-15 16:29:23 +0200

    folder name changed

---
+++ Account/Person.java
@@ -0,0 +1,16 @@
+/**
+ * Person
+ */
+public class Person {
+  private String name;
+  private String surname;
+  private String address;
+  private int account;
+  private String gender;
+
+
+
+
+
+
+}
