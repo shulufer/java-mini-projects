7b0cf4ad6830a48ed2502e6e8e86dc6ae3ae4ee2
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-07-17 20:16:18 +0200

    classroom availability setted

--- University/Classroom.java
+++ University/Classroom.java
@@ -6,5 +6,46 @@ public class Classroom {
   private int no;
   private int time;
 
+  Classroom(int no, int time) {
+    this.no = no;
+    this.time = time;
+  }
+
+
+  public int getNo() {
+    return this.no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public int getTime() {
+    return this.time;
+  }
+
+
+  public String settimePeriod(int time) {
+    this.time = time;
+    String timeLine;
+    switch (time) {
+      case 1:
+        timeLine = "08:00 - 11:00";
+        break;
+      case 2:
+        timeLine = "12:00 - 15:00";
+        break;
+      case 3:
+        timeLine = "16:00 - 19:00";
+        break;
+
+      default:
+        timeLine = "Timeline is not set";
+        break;
+    }
+    return timeLine;
+  }
+
+
 
 }
