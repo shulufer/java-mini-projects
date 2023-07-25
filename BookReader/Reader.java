79b10c9da7ce6c5fbd20af36d5706da738c2d0cd
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-07-25 22:31:41 +0200

    variables created

---
+++ BookReader/Reader.java
@@ -0,0 +1,21 @@
+/**
+ * Reader
+ */
+public class Reader {
+
+  private String name;
+  private int age;
+  private char sex;
+
+
+  public Reader(String name, int age, char sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+
+  public void Read() {
+
+  }
+
+}
---
+++ BookReader/ReaderApp.java
@@ -0,0 +1,5 @@
+public class ReaderApp {
+  public static void main(String[] args) {
+
+  }
+}
