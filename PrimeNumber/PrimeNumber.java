e7712df458058296cb163c6988ac9d3fc3a1b5cd
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-07-31 17:59:07 +0200

    prime methods fixed

--- PrimeNumber/PrimeNumber.java
+++ PrimeNumber/PrimeNumber.java
@@ -10,23 +10,37 @@ public class PrimeNumber {
       System.out.println("Input:");
       int number = sc.nextInt();
 
+      System.out.println("********");
       System.out.println(primeOrNot(number));
+      System.out.println("********");
 
 
       sc.close();
 
     }
 
-      public static int primeOrNot(int n) {
-        int prime = 0;
-        int upperBound = (int) (Math.sqrt(n) + 1);
-        for (int i = 2; i < upperBound; i++) {
-          if (n % i == 0) {
-            prime = 1;
-            break;
+      public static String primeOrNot(int number) {
+        String s = new String(" ");
+        int[] primesNumber = new int[number];
+        int k;
+        for (int i = (number - 1); i > 1; i--) {
+          k = 0;
+          for (int j = 2; j < i - 1; j++) {
+            if (i % j == 0) {
+              k++;
+            }
+
+          }
+          if (k == 0) {
+            primesNumber[i] = i;
           }
         }
-        return prime;
+        for (int i = 0; i < number; i++) {
+          if (primesNumber[i] != 0) {
+            s += primesNumber[i] + " ";
+          }
+        }
+        return s;
       }
 
     }
