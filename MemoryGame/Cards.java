9ed661e65e5b4965d1570086b7fc771c25af6706
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-08-08 19:23:46 +0200

    first raound created

--- MemoryGame/Cards.java
+++ MemoryGame/Cards.java
@@ -1,29 +1,65 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Cards {
 
-  char[][] backSide = new char[4][4];
-  char[][] frontSide = new char[4][4];
-  char[][] chars = {{'A'} ,{'B'},{'C'} ,{'D'},{'E'},{'F'},{'G'} , {'H'} ,
-  {'A'} ,{'B'},{'C'} ,{'D'},{'E'},{'F'},{'G'} , {'H'}};
-  int[][] numbers = {{1} ,{2},{3} ,{4},{5},{6},{7} , {8} ,
-  {9} ,{10},{11} ,{12},{13},{14},{15} , {16}};
+  private static char[][] backSide = new char[4][4];
+  static{
+    System.out.println("Start....");
+    for (int i = 0; i < backSide.length; i++) {
+          for (int j = 0; j < backSide.length; j++) {
+            backSide[i][j] = 'x';
 
-  public void startGame(){
+          }
+        }
+  }
+  private static char[][] frontSide = new char[4][4];
+  static {
+    frontSide[0][0] = 'A';
+    frontSide[0][1] = 'B';
+    frontSide[0][2] = 'C';
+    frontSide[0][3] = 'D';
+    frontSide[1][0] = 'E';
+    frontSide[1][1] = 'F';
+    frontSide[1][2] = 'G';
+    frontSide[1][3] = 'H';
+    frontSide[2][0] = 'A';
+    frontSide[2][1] = 'B';
+    frontSide[2][2] = 'C';
+    frontSide[2][3] = 'D';
+    frontSide[3][0] = 'E';
+    frontSide[3][1] = 'F';
+    frontSide[3][2] = 'G';
+    frontSide[3][3] = 'H';
+  }
+
+
+
+
+  public static void showCards(){
 
     for (int i = 0; i < backSide.length; i++) {
       for (int j = 0; j < backSide.length; j++) {
-        backSide[i][j] = 'x';
+        // String s = " " + backSide[i][j] + " ";
+        System.out.print( " " + backSide[i][j] + " ");
       }
+      System.out.println();
     }
-    for (int i = 0; i < frontSide.length; i++) {
-      for (int j = 0; j < frontSide.length; j++) {
+  }
 
-          frontSide[i][j] =
+  public static void firstRound(){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Press first x and y");
+    int x1 = scan.nextInt() - 1;
+    int y1 = scan.nextInt() - 1;
 
-      }
-    }
+    backSide[x1][y1]= frontSide[x1][y1];
+
 
   }
+
+
+
 
 
 }
--- MemoryGame/MemoryGame.java
+++ MemoryGame/MemoryGame.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
 
 public class MemoryGame {
 
   public static void main(String[] args) {
 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Welcome to Memory Game.");
+    String quitOrNot = "a";
+
+    do {
+      Cards.showCards();
+
+
+      Cards.firstRound();
+      Cards.showCards();
+
+      // System.out.println("Press second x");
+      // int x2 = scan.nextInt();
+      // System.out.println("Press second y");
+      // int y2 = scan.nextInt();
+      // scan.nextLine();
+
+      // Cards.showCards();
+
+      System.out.println("Press q for quite.");
+      quitOrNot = scan.nextLine();
+    } while (quitOrNot != "q");
+
+
+
+    scan.close();;
   }
+
+
 }
